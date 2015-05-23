a773e630b590da1987c2d430cb3381cefed6c1dd
Antoine <antoine@crafting-labs.fr>
2015-05-23 18:48:29 +0200

    let thing appear
    ok, except first and last line, others follow the same pattern :
     x spaces - a char - y spaces - same char - x spaces
    
    first and last lines are
     z spaces - A - z spaces
    
    if a diamond 'A' has a size of 1, 'B' of 2, 'C' 3 and so on, then z = size - 1
    
    lets do something with that

--- src/main/java/fr/craftinglabs/kata/Diamond.java
+++ src/main/java/fr/craftinglabs/kata/Diamond.java
@@ -4,17 +4,17 @@ public class Diamond {
 
     public static String create(Character c) {
         if (c.equals('C')) {
-            return "  A  \n"
-                 + " B B \n"
-                 + "C   C\n"
-                 + " B B \n"
-                 + "  A  ";
+            return "  " + "A" + "  "              + "\n"
+                 +  " " + "B" + " "   + "B" + " " + "\n"
+                 +   "" + "C" + "   " + "C" + ""  + "\n"
+                 + " "  + "B" + " "   + "B" + " " + "\n"
+                 + "  " + "A" + "  ";
         }
         
         if (c.equals('B')) {
-            return " A \n"
-                 + "B B\n"
-                 + " A ";
+            return " " + "A" + " " + "\n"
+                 + "B" + " " + "B" + "\n"
+                 + " " + "A" + " ";
         }
         
         return "A";

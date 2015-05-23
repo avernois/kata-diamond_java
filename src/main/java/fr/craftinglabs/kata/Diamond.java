6575b46ab16743f753d2e59db681866eac5b5e58
Antoine <antoine@crafting-labs.fr>
2015-05-23 19:03:20 +0200

    let the diamond tip appear

--- src/main/java/fr/craftinglabs/kata/Diamond.java
+++ src/main/java/fr/craftinglabs/kata/Diamond.java
@@ -3,21 +3,35 @@ package fr.craftinglabs.kata;
 public class Diamond {
 
     public static String create(Character c) {
+        int size = c - 'A' + 1;
+        
         if (c.equals('C')) {
-            return "  " + "A" + "  "              + "\n"
+            return diamondTip(size) + "\n"
                  +  " " + "B" + " "   + "B" + " " + "\n"
                  +   "" + "C" + "   " + "C" + ""  + "\n"
                  + " "  + "B" + " "   + "B" + " " + "\n"
-                 + "  " + "A" + "  ";
+                 + diamondTip(size);
         }
         
         if (c.equals('B')) {
-            return " " + "A" + " " + "\n"
+            return diamondTip(size) + "\n"
                  + "B" + " " + "B" + "\n"
-                 + " " + "A" + " ";
+                 + diamondTip(size);
         }
         
         return "A";
     }
 
+    private static String diamondTip(int size) {
+        return manySpaces(size -1) + "A" + manySpaces(size - 1);
+    }
+
+    private static String manySpaces(int nbSpaces) {
+        StringBuilder builder = new StringBuilder();
+        
+        for (int i = 0; i < nbSpaces; i++) {
+            builder.append(" ");
+        }
+        return builder.toString();
+    }
 }

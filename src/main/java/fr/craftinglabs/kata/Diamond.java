bc8a21d5febf38d19f4984c3ff5e0c8c50d72c5b
Antoine <antoine@crafting-labs.fr>
2015-05-23 18:40:54 +0200

    Diamond should create a B diamond
    I can't really see any pattern emerging. Let's go to 'C'

--- src/main/java/fr/craftinglabs/kata/Diamond.java
+++ src/main/java/fr/craftinglabs/kata/Diamond.java
@@ -3,6 +3,12 @@ package fr.craftinglabs.kata;
 public class Diamond {
 
     public static String create(Character c) {
+        if (c.equals('B')) {
+            return " A \n"
+                 + "B B\n"
+                 + " A ";
+        }
+        
         return "A";
     }
 
--- src/test/java/fr/craftinglabs/kata/DiamondTest.java
+++ src/test/java/fr/craftinglabs/kata/DiamondTest.java
@@ -10,4 +10,9 @@ public class DiamondTest {
     should_draw_diamond_A() {
         assertEquals("A", Diamond.create('A'));
     }
+
+    @Test public void 
+    should_draw_diamond_B() {
+        assertEquals(" A \nB B\n A ", Diamond.create('B'));
+    }
 }

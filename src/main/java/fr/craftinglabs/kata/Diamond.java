d391b682234b1783d139e3a2fe33ca9a91298631
Antoine <antoine@crafting-labs.fr>
2015-05-23 18:42:12 +0200

    Diamond should create a C diamond
    Ok, I start to see things. Let's make them appear

--- src/main/java/fr/craftinglabs/kata/Diamond.java
+++ src/main/java/fr/craftinglabs/kata/Diamond.java
@@ -3,6 +3,14 @@ package fr.craftinglabs.kata;
 public class Diamond {
 
     public static String create(Character c) {
+        if (c.equals('C')) {
+            return "  A  \n"
+                 + " B B \n"
+                 + "C   C\n"
+                 + " B B \n"
+                 + "  A  ";
+        }
+        
         if (c.equals('B')) {
             return " A \n"
                  + "B B\n"
--- src/test/java/fr/craftinglabs/kata/DiamondTest.java
+++ src/test/java/fr/craftinglabs/kata/DiamondTest.java
@@ -15,4 +15,14 @@ public class DiamondTest {
     should_draw_diamond_B() {
         assertEquals(" A \nB B\n A ", Diamond.create('B'));
     }
+
+    @Test public void 
+    should_draw_diamond_C() {
+        assertEquals("  A  \n"
+                   + " B B \n"
+                   + "C   C\n"
+                   + " B B \n"
+                   + "  A  ", Diamond.create('C'));
+    }
+    
 }
